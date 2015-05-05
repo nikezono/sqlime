da7d4717d184c676bd490aba332dae7e7c82e9b3
stkay <doraaki0@gmail.com>
2015-05-05 09:26:13 +0900

    return empty array when sqlite is 👺

--- app/src/main/java/net/nikezono/sqlime/translate/dictionary/SQLite3DictionaryAccesor.java
+++ app/src/main/java/net/nikezono/sqlime/translate/dictionary/SQLite3DictionaryAccesor.java
@@ -3,6 +3,7 @@ package net.nikezono.sqlime.translate.dictionary;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.util.Log;
 
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
@@ -38,39 +39,43 @@ public class SQLite3DictionaryAccesor extends SQLiteAssetHelper {
     // @todo Viewが末端までスクロールされたらページングする
     @DebugLog
     public ArrayList<String> getWordsByYomigana(String yomigana, int limit, int offset) {
-        mDatabase.beginTransaction();
-        int length = yomigana.length();
-        StringBuilder spaced = new StringBuilder();
-        for(int i=0; length> i; i++){
-            spaced.append(yomigana.charAt(i));
-            if(i+1 != length) {
-                spaced.append(" NEAR/1 ");
+        try {
+            mDatabase.beginTransaction();
+            int length = yomigana.length();
+            StringBuilder spaced = new StringBuilder();
+            for (int i = 0; length > i; i++) {
+                spaced.append(yomigana.charAt(i));
+                if (i + 1 != length) {
+                    spaced.append(" NEAR/1 ");
+                }
             }
+
+            String query = statement
+                    .replace("?", spaced)
+                    .replace("$", Integer.toString(limit))
+                    .replace("#", Integer.toString(offset));
+            Log.d("query", query);
+            Cursor cursor = mDatabase.rawQuery(query, null);
+            mDatabase.endTransaction();
+
+            //カーソル開始位置を先頭にする
+            cursor.moveToFirst();
+
+            mResultSet.clear();
+            int length2 = cursor.getCount();
+            //（.moveToFirstの部分はとばして）for文を回す
+            for (int i = 1; i <= length2; i++) {
+                //SQL文の結果から、必要な値を取り出す
+                String word = cursor.getString(0);
+                String yomi = cursor.getString(1);
+                int score = cursor.getInt(2);
+                mResultSet.add(word);
+                cursor.moveToNext();
+            }
+            return mResultSet;
+        } catch (SQLiteException ex) {
+            return new ArrayList<String>();
         }
-
-        String query = statement
-                .replace("?", spaced)
-                .replace("$", Integer.toString(limit))
-                .replace("#", Integer.toString(offset));
-        Log.d("query",query);
-        Cursor cursor = mDatabase.rawQuery(query,null);
-        mDatabase.endTransaction();
-
-        //カーソル開始位置を先頭にする
-        cursor.moveToFirst();
-
-        mResultSet.clear();
-        int length2 = cursor.getCount();
-        //（.moveToFirstの部分はとばして）for文を回す
-        for (int i = 1; i <= length2; i++) {
-            //SQL文の結果から、必要な値を取り出す
-            String word = cursor.getString(0);
-            String yomi = cursor.getString(1);
-            int score = cursor.getInt(2);
-            mResultSet.add(word);
-            cursor.moveToNext();
-        }
-        return mResultSet;
     }
 
     private final static String statement =
